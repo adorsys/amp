565e5b1ac45b97eda1ab7470b6ebdfab6c96c22c
Alexander Geist <age@adorsys.de>
2015-11-13 11:20:48 +0100

    configurable proxy

--- amp-client/src/main/java/de/adorsys/amp/gcm/client/GCMService.java
+++ amp-client/src/main/java/de/adorsys/amp/gcm/client/GCMService.java
@@ -5,80 +5,102 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 
 import de.adorsys.amp.gcm.client.GCMMessage.GCMNotification;
 import de.adorsys.amp.gcm.client.GCMResults.GCMResult;
 
 @Singleton
 public class GCMService {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(GCMService.class);
-	
-	private static com.fasterxml.jackson.databind.ObjectMapper OM = new com.fasterxml.jackson.databind.ObjectMapper();
-	
-	static {
-		Unirest.setObjectMapper(new ObjectMapper() {
-			
+
+	private HttpHost proxyHost;
+	private ObjectMapper objectMapper;
+
+	@PostConstruct
+	public void postConstruct() {
+		String proxyHostUrl = System.getProperty("GCM_PROXY_HOST");
+		String proxyPort = System.getProperty("GCM_PROXY_PORT");
+
+		if (proxyHostUrl != null) {
+			if (proxyPort != null) {
+				proxyHost = new HttpHost(proxyHostUrl, Integer.valueOf(proxyPort));
+			} else {
+				proxyHost = new HttpHost(proxyHostUrl);
+			}
+		}
+
+		final com.fasterxml.jackson.databind.ObjectMapper OM = new com.fasterxml.jackson.databind.ObjectMapper();
+		objectMapper = new ObjectMapper() {
+
 			@Override
 			public String writeValue(Object value) {
 				try {
 					return OM.writeValueAsString(value);
 				} catch (JsonProcessingException e) {
-					throw new GCMException("Connection problem sending GCM Message", e);
+					throw new GCMException("problem on selialization of " + value);
 				}
 			}
-			
+
 			@Override
 			public <T> T readValue(String value, Class<T> valueType) {
 				try {
 					return OM.readValue(value, valueType);
 				} catch (IOException e) {
-					throw new GCMException("Connection problem sending GCM Message", e);
+					throw new GCMException("problem on deselialization of " + value);
 				}
 			}
-		});
+		};
+
+		Unirest.setObjectMapper(objectMapper);
 	}
-	
-	public void sendNotification(GCMNotification notification, Map<String, Object> data, String gcmApiKey, String... registrationIds) throws UnknownRegistrationIdException, NotRegisteredException {
+
+	public void sendNotification(GCMNotification notification, Map<String, Object> data, String gcmApiKey, String... registrationIds)
+			throws UnknownRegistrationIdException, NotRegisteredException {
 		GCMMessage gcmMessage = new GCMMessage();
-		
+
 		Map<String, String> converted = new HashMap<>();
 		for (Map.Entry<String, Object> entry : data.entrySet()) {
-			try {
-				converted.put(entry.getKey(), OM.writeValueAsString(entry.getValue()));
-			} catch (JsonProcessingException e) {
-				throw new GCMException("problem on selialization of "+ entry.getValue());
-			}
+			converted.put(entry.getKey(), objectMapper.writeValue(entry.getValue()));
 		}
 		gcmMessage.setData(converted);
 		gcmMessage.setRegistrationIds(registrationIds);
 		gcmMessage.setNotification(notification);
-		try {
-			HttpResponse<GCMResults> results = Unirest.post("https://gcm-http.googleapis.com/gcm/send")
-					  .header("Authorization", "key=" + gcmApiKey)
-					  .header("Content-Type", "application/json")
-					  .header("accept", "application/json")
-					  .body(gcmMessage)
-					  .asObject(GCMResults.class);
+
+		HttpPost httpPost = new HttpPost("https://gcm-http.googleapis.com/gcm/send");
+		httpPost.addHeader("Authorization", "key=" + gcmApiKey);
+		httpPost.addHeader("Content-Type", "application/json");
+		httpPost.addHeader("accept", "application/json");
+		httpPost.setEntity(new StringEntity(objectMapper.writeValue(gcmMessage), "UTF-8"));
+
+		try (CloseableHttpClient httpClient = HttpClients.custom().setProxy(proxyHost).build();
+				CloseableHttpResponse closeableHttpResponse = httpClient.execute(httpPost);) {
+
+			HttpResponse<GCMResults> results = new HttpResponse<>(closeableHttpResponse, GCMResults.class);
+
+			// HttpResponse<GCMResults> results = Unirest.post("https://gcm-http.googleapis.com/gcm/send").header("Authorization", "key=" + gcmApiKey)
+			// .header("Content-Type", "application/json").header("accept", "application/json").body(gcmMessage).asObject(GCMResults.class);
 			List<GCMResult> resultList = results.getBody().getResults();
 			for (GCMResult gcmResult : resultList) {
-				if ("InvalidRegistration".equals(gcmResult.getError())){
+				if ("InvalidRegistration".equals(gcmResult.getError())) {
 					throw new UnknownRegistrationIdException(registrationIds[0]);
 				} else if ("NotRegistered".equals(gcmResult.getError())) {
 					throw new NotRegisteredException(registrationIds[0]);
 				}
 			}
-		} catch (UnirestException e) {
+		} catch (IOException e) {
 			throw new GCMException("Connection problem sending GCM Message", e);
 		}
 	}
